1e189e179ef5b22b5c6e86e2d69a6cdc4b1d1144
yuri-ramos <yroramos@gmail.com>
2023-04-03 11:36:46 -0300

    Criando um script de execução

---
+++ Angula/TypeScript para Angular/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function printaObjeto(pessoa) {
+    console.log(pessoa);
+}
+printaObjeto({
+    name: "Yurizao",
+    vulgo: "o gostosao",
+});
+console.log("hellow dark my oud friend");
---
+++ Angula/TypeScript para Angular/src/index.ts
@@ -0,0 +1,14 @@
+type heroi={
+    name: string;
+    vulgo: string;
+}
+
+function printaObjeto(pessoa: heroi) {
+    console.log(pessoa);
+}
+printaObjeto({
+    name: "Yurizao",
+    vulgo: "o gostosao",
+})
+
+console.log("hellow dark my oud friend");
